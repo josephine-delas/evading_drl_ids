cede5b4c850e67ae0e1f9022f259ade798144492
josephine-delas <delasjosephine@gmail.com>
2023-04-24 14:10:39 -0600

    Adding fgm attack in train_multiple_agent.py

--- scripts/train_multiple_agents.py
+++ scripts/train_multiple_agents.py
@@ -5,11 +5,16 @@ import os
 import argparse
 import numpy as np
 from sklearn.metrics import f1_score
+import torch.nn as nn
+import torch
 
 from pathlib import Path
 import sys
 path_root = Path(__file__).parents[1]
 sys.path.append(str(path_root))
+
+from art.attacks.evasion import FastGradientMethod
+from art.estimators.classification import PyTorchClassifier
 
 from ids_env.common.config_workspace import config_device, config_seed
 from ids_env.common.config_ids_env import make_training_env, make_multi_proc_training_env, make_testing_env
@@ -19,6 +24,7 @@ from ids_env.common.utils import calcul_rates, print_stats
 if __name__=='__main__':
 
     ####----Parameters----####
+
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--data", required=True, default="KDD", help="Dataset to use: 'KDD' or 'AWID'.")
     parser.add_argument("-m", "--model", required=True, default="DQN", help="RL algorithm: 'DQN' or 'PPO'.")
@@ -39,6 +45,7 @@ if __name__=='__main__':
     nb_agents = args.nb_agents
     binary = args.binary
     verbose = False
+    epsilon_range=[0.005, 0.01, 0.02, 0.05, 0.07, 0.1]
 
     # Workspace config parameters
     device_name = 'cpu' 
@@ -53,7 +60,7 @@ if __name__=='__main__':
     device = config_device(device_name)
     config_seed(seed)
 
-    ####----Evaluation----####
+    ####----Evaluation Variables----####
 
     testing_env = make_testing_env(dataset, binary=binary)() 
     training_env= make_training_env(dataset, binary=binary)()
@@ -64,20 +71,24 @@ if __name__=='__main__':
     dict_attack=dict((testing_env.attack_types[i], i) for i in range(len(testing_env.attack_types)))
     test_labels=testing_env.y.replace(dict_attack)
     train_labels=training_env.y.replace(dict_attack)
+    nb_class = len(testing_env.attack_types)
     if binary:
         test_labels = np.sign(test_labels)
         train_labels = np.sign(train_labels)
+        nb_class=2
 
 
-    ####---Loop----#
+    ####---Loop----####
+
     best_fpr=1
     best_fnr=1
     best_f1_score=0
     test_fpr=np.zeros(nb_agents)
     test_fnr=np.zeros(nb_agents)
     test_f1=np.zeros(nb_agents)
+
     for i in range(nb_agents):
-        print('Started training of agent {} / {}'.format(i, nb_agents))
+        print('Started training of agent {} / {}'.format(i+1, nb_agents))
 
         vectorized_training_env = make_multi_proc_training_env(nb_proc=nb_proc, dataset=dataset, binary=binary)
 
@@ -88,7 +99,8 @@ if __name__=='__main__':
 
         # Training
 
-        agent.learn(testing_env, n_envs=nb_proc, save_dir=output_dir, num_epoch=epochs)
+        #agent.learn(testing_env, n_envs=nb_proc, save_dir=output_dir, num_epoch=epochs)
+        agent.load(output_dir+'/last_model.zip')
 
         if model=='DQN':
             agent.model.policy.set_training_mode(False) # Switch to testing mode
@@ -102,8 +114,40 @@ if __name__=='__main__':
             test_f1[i] = f1_score(test_labels, test_actions)
         else:
             test_f1[i] = f1_score(test_labels, test_actions, average='weighted')
+        
+        # Verbose 
+        #if binary : 
+        #    print_stats(['Normal', 'Attack'], test_labels, test_actions)
+        #else:
+        #    print_stats(testing_env.attack_types, test_labels, test_actions)
+        #print('FPR: ' + str(test_fpr[i]) + ', FNR: ' + str(test_fnr[i]) + ', F1_avg: ' +str(test_f1[i]))
 
-        print('Completed training of agent {} / {}'.format(i, nb_agents))
+        print('Completed training of agent {} / {}'.format(i+1, nb_agents))
+
+        ####----Adversarial Attack----####
+        pytorch_model = agent.get_pytorch_model()
+        classifier = PyTorchClassifier(model = pytorch_model, loss=nn.HuberLoss(), 
+                                       input_shape=test_set.shape[1], nb_classes=nb_class)
+
+        for epsilon in epsilon_range:
+            fgm = FastGradientMethod(classifier,
+                                         norm=np.inf,
+                                         eps=epsilon,
+                                         targeted=True,
+                                         num_random_init=0,
+                                         batch_size=128,
+                                         )
+            adversarial_examples = fgm.generate(x=test_set, y=np.hstack((np.ones((test_set.shape[0], 1)), np.zeros((test_set.shape[0], nb_class-1)))).astype('float32'))# we assume the normal class is the first column
+            adversarial_actions = agent.model.predict(adversarial_examples, deterministic=True)[0]
+            
+            # TODO : add other attacks / Save metrics in numpy arrray
+            
+            # Verbose 
+            #print('Adversarial Attack:')
+            #if binary : 
+            #    print_stats(['Normal', 'Attack'], test_labels, adversarial_actions)
+            #else:
+            #    print_stats(testing_env.attack_types, test_labels, adversarial_actions)
 
     # Saving model and evaluation metrics
     if not os.path.exists(output_dir):
